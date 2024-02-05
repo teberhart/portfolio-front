e98eb4ee3f8db26c632765c752417627f1858015
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 19:45:35 +0100

    added base for project handling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,41 @@
 import styles from './Main.module.css';
 import ProjectList from "./Projects/ProjectList.jsx";
 import SkillsList from "./Skills/SkillsList.jsx";
+import ClientsList from "./Clients/ClientsList.jsx";
+import {useState} from "react";
 
 export default function Main() {
+	const [projectArray, setProjects] = useState([]);
+	async function fetchProjects() {
+		const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
+		const apiProjectEndpoint = apiEndpoint + '/projects';
+		const projectList = await fetch(apiProjectEndpoint);
+		const data = await projectList.json();
+		setProjects(data);
+
+		console.log(apiProjectEndpoint);
+	}
+
+	async function handleButton(e) {
+		e.preventDefault();
+		const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
+		const apiProjectEndpoint = apiEndpoint + '/projects';
+		const projectList = await fetch(apiProjectEndpoint);
+		const data = await projectList.json();
+
+		console.log(data);
+	}
 	return (
 		<div className={styles.appBody}>
-			<div>
-				<ProjectList />
+			<button onClick={(e)=>handleButton(e)}></button>
+			<div className={styles.projectsList}>
+				<ProjectList list={projectArray}/>
 			</div>
 			<div>
 				<SkillsList />
+			</div>
+			<div>
+				<ClientsList />
 			</div>
 		</div>
 	);
--- src/components/Projects/ProjectList.jsx
+++ src/components/Projects/ProjectList.jsx
@@ -1,5 +1,65 @@
-export default function ProjectList() {
+import 'react-multi-carousel/lib/styles.css';
+import styles from './ProjectList.module.css';
+import Carousel from "react-multi-carousel";
+
+export default function ProjectList({list}) {
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 3
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1
+		}
+	};
 	return (
-		<div></div>
+		<div>
+			<div>
+				{list.map((project)=>"Projet")}
+			</div>
+			<div className={styles.carousel}>
+				<Carousel
+					swipeable={false}
+					draggable={true}
+					showDots={true}
+					responsive={responsive}
+					ssr={true} // means to render carousel on server-side.
+					infinite={true}
+					autoPlay={false}
+					autoPlaySpeed={1000}
+					keyBoardControl={true}
+					customTransition="all .5"
+					transitionDuration={500}
+					containerClass="carousel-container"
+					removeArrowOnDeviceType={["tablet", "mobile"]}
+					dotListClass="custom-dot-list-style"
+					itemClass="carousel-item-padding-40-px"
+					centerMode={true}
+				>
+					<div>
+						<img src="https://avatars.githubusercontent.com/u/13420081?s=280&v=4" />
+					</div>
+					<div>
+						<img src="https://api-platform.com/docs/images/logo_spider.svg"/>
+					</div>
+					<div>
+						<img src="https://avatars.githubusercontent.com/u/13420081?s=280&v=4"/>
+					</div>
+					<div>
+						<img src="https://api-platform.com/docs/images/logo_spider.svg"/>
+					</div>
+				</Carousel>;
+			</div>
+		</div>
 	);
 }
