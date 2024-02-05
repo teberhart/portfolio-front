51ff4c14bb1f6facad6e4e1ebb0e945d2b3146e6
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 17:35:52 +0100

    basic structure done : header, footer and body

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+	return (
+		<footer>
+			<div className={styles.main}>
+				This is a footer
+			</div>
+		</footer>
+	);
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import styles from './Main.module.css';
+
+export default function Main() {
+	return (
+		<div className={styles.appBody}>
+			This is the main body of the app
+		</div>
+	);
+}
