91336757f017994001d25fc625851ec05663943d
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 17:45:23 +0100

    added base for projects and skills

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,16 @@
 import styles from './Main.module.css';
+import ProjectList from "./Projects/ProjectList.jsx";
+import SkillsList from "./Skills/SkillsList.jsx";
 
 export default function Main() {
 	return (
 		<div className={styles.appBody}>
-			This is the main body of the app
+			<div>
+				<ProjectList />
+			</div>
+			<div>
+				<SkillsList />
+			</div>
 		</div>
 	);
 }
